2f0f8f833a075e6141fc40fd7884ca1e59a47589
aashna <aasgup312@gmail.com>
2023-10-15 15:04:10 -0700

    test
    not done

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/javastick_driving.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="javastick_driving", group="Exercises")
+//@Disabled
+public class DriveTank extends LinearOpMode
+{
+    DcMotor leftMotor, rightMotor;
+    float   leftY, rightY;
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        // wait for start button.
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            leftY = gamepad1.left_stick_y * -1;
+            rightY = gamepad1.right_stick_y * -1;
+
+            leftMotor.setPower(Range.clip(leftY, -1.0, 1.0));
+            rightMotor.setPower(Range.clip(rightY, -1.0, 1.0));
+
+            telemetry.addData("Mode", "running");
+            telemetry.addData("sticks", "  left=" + leftY + "  right=" + rightY);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
